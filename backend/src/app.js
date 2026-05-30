f22a77d9c1030f122deb732b9a29a92a1fe90aa2
harshagrawal-coder <harshagrawal9498@gmail.com>
2026-05-30 14:22:11 +0530

    updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,14 +8,25 @@ import morgan from "morgan";
 import { generalLimiter } from "./middleware/rateLimiter.js";
 const app = express();
 
-const corsOptions = {
-  origin:"https://perplexity-silk.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
 
 app.use(helmet());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://perplexity-silk.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+    ],
+  })
+);
+
+
 
 app.use(cookieParser());
 app.use(express.json());
