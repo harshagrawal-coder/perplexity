48dc6e5479e7b95d7b0b2b380e1e599f4775738a
harshagrawal-coder <harshagrawal9498@gmail.com>
2026-05-30 14:15:29 +0530

    upt

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = "https://perplexity-backend-i9mw.onrender.com/api";
+import.meta.env.VITE_API_BASE_URL;
